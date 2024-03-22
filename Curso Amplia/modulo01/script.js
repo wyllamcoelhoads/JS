39bf2ffb50fe043fb6152417a7091ff69ce242e3
William Coelho <wyllamcoelho8@gmail.com>
2024-03-22 11:35:14 -0300

    sobre os operadores logicos em js

--- Curso Amplia/modulo01/script.js
+++ Curso Amplia/modulo01/script.js
@@ -1,4 +1,4 @@
-var mensagem = "Ola mundo";
+/*var mensagem = "Ola mundo";
 const mensagem2 = "Ola";
 let mensgaem3 = "olá mundo";
 mensagem4 = 'Ola pela ultima vez';
@@ -7,5 +7,20 @@ console.log(mensagem)
 
 mensagem = "novo valor"
 console.log(mensagem)
+*/
+
+// exemplos de uso dos operadores logicos
+
+x = 2;
+
+expressao_1 =  (x % 2 == 0) //é par?
+expressao_2 = (x % 2 == 1) // é impar?
+/*
+console.log(expressao_1) // false
+console.log(expressao_2) // true
+*/
+//comparanto as duas expressoees ao mesmos tempo
+
+console.log( expressao_1 && expressao_2) // false
 
 
