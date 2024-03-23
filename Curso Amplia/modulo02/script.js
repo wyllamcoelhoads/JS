fb48daad6935dc7da3ec860b622b26e3a220f816
William Coelho <wyllamcoelho8@gmail.com>
2024-03-23 12:55:32 -0300

    aula de script java kk

--- Curso Amplia/modulo02/script.js
+++ Curso Amplia/modulo02/script.js
@@ -1,7 +1,25 @@
 // modulo 02
+/*
+var x = 11;
 
-var x = 1;
-
-if((x % 2 == 1) || (x > 0)){
-    console.log("Numero impar ou positivo");
+if(x % 2 == 0){
+    console.log(x + " é numero par!");
+}else{
+    console.log(x + " é impar!");
 }
+*/
+//testando uma condição com tipos não numericos
+
+idade = "6";
+
+if(idade >= 4 && idade <6){
+    console.log("Idade " + idade + " Padrão, Pré-escolar");
+} else if(idade >= 6 && idade <15){
+    console.log("Idade " + idade + " Padrão, Ensino fundamental");
+} else if (idade >= 15 && idade < 18){
+    console.log("Idade " + idade + " Padrão, Ensino médio");
+}else{
+    console.log("Idade " + idade + " fora do padrão escolar");
+}
+console.log("*************************");
+console.log("Fim do validação")
